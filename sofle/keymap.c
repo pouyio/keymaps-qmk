b591756e8edfbda23040e7bbd79c0a1a960b0295
pouyio <pouyio@gmail.com>
2022-06-12 18:21:17 +0200

    added hold Z for ctrl in macos

--- sofle/keymap.c
+++ sofle/keymap.c
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L |   Ñ   |  ´   |
  * |------+------+------+------+------+------|  Mute |    |  Play |------+------+------+------+------+------|
- * | LAlt |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |  -  | RSft  |
+ * | LAlt |Z-Ctrl|   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |  -  | RSft  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            |      |      | LGUI |MLOWER| / Space /       \Enter \  |MRAISE| FDel |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_LSFT,  KC_A,  KC_S,  KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,  ES_NTIL, KC_QUOT,
-  KC_LALT,  KC_Z,   KC_X,  KC_C,   KC_V,   KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  KC_LALT, LCTL_T(KC_Z), KC_X,  KC_C,   KC_V,   KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  XXXXXXX,XXXXXXX, KC_LGUI, LT(_M_LOWER, KC_ESC), KC_SPC,      KC_ENT,   LT(_M_RAISE, ES_QUOT), KC_DELETE,    XXXXXXX, XXXXXXX
 ),
 /* LOWER

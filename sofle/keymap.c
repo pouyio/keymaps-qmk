d543e6dd81e4d6520f252b039803a835305f9046
pouyio <pouyio@gmail.com>
2022-11-14 08:58:11 +0100

    removed mac/win layers

--- sofle/keymap.c
+++ sofle/keymap.c
@@ -16,9 +16,7 @@ enum sofle_layers {
     _QWERTY,
     _M_QWERTY,
     _LOWER,
-    _M_LOWER,
     _RAISE,
-    _M_RAISE,
     _ADJUST,
 };
 
@@ -33,7 +31,13 @@ enum custom_keycodes {
     KC_C_BRK,  // [ and SHIFT([) = ]
     KC_C_PAR,  // ( and SHIFT(() = )
     KC_VIRG, // ~ for wind/mac
-    C_BSLS // \ backslash for win/mac
+    C_BSLS, // \ backslash for win/mac
+    C_LST, // < for mac/win
+    WDEL, // word delete for mac/win
+    PRVWD, // previous word for mac/win
+    NXTWD, // next word for mac/win
+    C_HOME, // home for mac/win
+    C_END, // end for mac/win
 };
 
 
@@ -53,45 +57,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_M_QWERTY] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
-  XXXXXXX,  KC_A,  KC_S,  KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    PLUS, XXXXXXX,
+  XXXXXXX,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+  XXXXXXX,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    PLUS, XXXXXXX,
   XXXXXXX,LSFT_T(KC_Z),LALT_T(KC_X),  KC_C,   KC_V,   KC_B, KC_MUTE,    KC_MPLY, KC_N,    KC_M, KC_COMM,LALT_T(KC_DOT), RSFT_T(KC_SLSH),XXXXXXX,
-  XXXXXXX, XXXXXXX,LGUI_T(KC_ESC),LT(_M_LOWER, KC_TAB),KC_SPC,KC_ENT,LT(_M_RAISE, KC_BSPC),  KC_DELETE, XXXXXXX, XXXXXXX
+  XXXXXXX, XXXXXXX,LGUI_T(KC_ESC),LT(_LOWER, KC_TAB),KC_SPC,KC_ENT,LT(_RAISE, KC_BSPC),  KC_DELETE, XXXXXXX, XXXXXXX
 ),
 [_LOWER] = LAYOUT(
   _______, _______, _______, _______, _______ , _______,                     _______, _______, _______, _______, _______, _______,
-  _______,  KC_TAB, _______, PRV_WPC,  NXT_WPC, _______,                     S(ES_6), S(ES_7),KC_C_PAR, S(ES_9), S(ES_0), _______,
-  _______, _______, _______, _______,KC_C_WINDOW,ES_QUOT,                    S(ES_1), KC_NUBS,KC_C_CBR,KC_C_BRK,S(ES_QUOT),_______,
-  _______, KC_LSFT, _______,KC_C_TAB_PREV,KC_C_TAB,ES_GRV,_______,  _______,KC_RBRC,S(ES_2),S(KC_COMM),S(KC_DOT),S(ES_MINS),_______,
+  _______, KC_VOLU, KC_MUTE, PRV_WPC,  NXT_WPC, _______,                     S(ES_6), S(ES_7),KC_C_PAR, S(ES_9), S(ES_0), _______,
+  _______, KC_VOLD, KC_MPLY, _______,KC_C_WINDOW,ES_QUOT,                    S(ES_1),   C_LST,KC_C_CBR,KC_C_BRK,S(ES_QUOT),_______,
+  _______, KC_LSFT, _______,KC_C_TAB_PREV,KC_C_TAB,ES_GRV,_______,  _______, KC_RBRC, S(ES_2),S(KC_COMM),S(KC_DOT),S(ES_MINS),_______,
                     _______, _______, _______, _______, _______,    _______, LT(_RAISE, KC_NO),WDEL,_______, _______
-),
-[_M_LOWER] = LAYOUT(
-  _______, _______, _______, _______, _______ , _______,                     _______, _______, _______, _______, _______, _______,
-  _______,  KC_TAB, _______, PRV_WPC,  NXT_WPC, _______,                     S(ES_6), S(ES_7),KC_C_PAR, S(ES_9), S(ES_0), _______,
-  _______, _______, _______, _______,KC_C_WINDOW,ES_QUOT,                  S(ES_1),KC_GRV,KC_C_CBR,KC_C_BRK,S(ES_QUOT), _______,
-  _______, KC_LSFT, _______,KC_C_TAB_PREV,KC_C_TAB,ES_GRV, _______,  _______,KC_RBRC,S(ES_2),S(KC_COMM),S(KC_DOT),S(ES_MINS), _______,
-                    _______, _______, _______, _______, _______,     _______,LT(_M_RAISE, KC_NO),M_WDEL,_______,_______
 ),
 [_RAISE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
   _______,ALGR(ES_1),ALGR(ES_2),ALGR(ES_3),S(ES_4),S(ES_5),                 _______,   PRVWD,   KC_UP,   NXTWD, _______, _______,
   _______, _______, _______, _______, _______, _______,                     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  _______, _______, _______, _______, _______, _______,  _______,  _______, _______, KC_HOME, _______,  KC_END, _______, _______,
+  _______, _______, _______, _______, _______, _______,  _______,  _______, _______,  C_HOME, _______,   C_END, _______, _______,
                     _______, _______, _______,MO(_LOWER),_______,  _______, _______, _______, _______, _______
 ),
-[_M_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______,
-  _______,ALGR(ES_1),ALGR(ES_2),ALGR(ES_3),S(ES_4),S(ES_5),                  _______, M_PRVWD,   KC_UP, M_NXTWD, _______, _______,
-  _______, _______, _______, _______, _______, _______,                      _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  _______, _______, _______, _______, _______, _______,  _______,   _______, _______,  M_HOME, _______,   M_END, _______, _______,
-                    _______, _______, _______,MO(_M_LOWER),_______, _______, _______, _______, _______, _______
-),
-//  ALGR(ES_NTIL) no va en windows, sólo en linux
 [_ADJUST] = LAYOUT(
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX, KC_EQL,KC_QWERTY,ALGR(ES_E),XXXXXXX,KC_PLUS,                      XXXXXXX,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
   XXXXXXX, C_BSLS,KC_M_QWERTY,XXXXXXX,XXXXXXX, XXXXXXX,                      XXXXXXX,    KC_4,    KC_5,    KC_6, KC_VIRG, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, ES_NTIL,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
                     _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
   )
 };
@@ -108,8 +97,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     // update _ADJUST layer
     if (IS_LAYER_ON(_LOWER) || IS_LAYER_ON(_RAISE)) {
         state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-    } else if (IS_LAYER_ON(_M_LOWER) || IS_LAYER_ON(_M_RAISE)) {
-        state = update_tri_layer_state(state, _M_LOWER, _M_RAISE, _ADJUST);
     }
     return state;
 }
@@ -242,22 +229,122 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case C_BSLS:
             if (record->event.pressed) {
                 if (isWin) {
-                    tap_code16(ALGR(ES_MORD));
+                    register_code16(ALGR(ES_MORD));
                 } else {
-                    tap_code16(LALT(KC_GRV));
+                    register_code16(LALT(KC_GRV));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(ALGR(ES_MORD));
+                } else {
+                    unregister_code16(LALT(KC_GRV));
+                }
+            }
+            return true;
+        case C_LST:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(KC_NUBS);
+                } else {
+                    register_code16(KC_GRV);
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(KC_NUBS);
+                } else {
+                    unregister_code16(KC_GRV);
+                }
+            }
+            return true;
+        case WDEL:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(LCTL(KC_DELETE));
+                } else {
+                    register_code16(LALT(KC_DELETE));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(LCTL(KC_DELETE));
+                } else {
+                    unregister_code16(LALT(KC_DELETE));
+                }
+            }
+            return true;
+        case PRVWD:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(LCTL(KC_LEFT));
+                } else {
+                    register_code16(LALT(KC_LEFT));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(LCTL(KC_LEFT));
+                } else {
+                    unregister_code16(LALT(KC_LEFT));
+                }
+            }
+            return true;
+        case NXTWD:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(LCTL(KC_RIGHT));
+                } else {
+                    register_code16(LALT(KC_RIGHT));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(LCTL(KC_RIGHT));
+                } else {
+                    unregister_code16(LALT(KC_RIGHT));
+                }
+            }
+            return true;
+        case C_HOME:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(KC_HOME);
+                } else {
+                    register_code16(RCMD(KC_LEFT));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(KC_HOME);
+                } else {
+                    unregister_code16(RCMD(KC_LEFT));
+                }
+                
+            }
+            return true;
+        case C_END:
+            if (record->event.pressed) {
+                if (isWin) {
+                    register_code16(KC_END);
+                } else {
+                    register_code16(RCMD(KC_RIGHT));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(KC_END);
+                } else {
+                    unregister_code16(RCMD(KC_RIGHT));
                 }
             }
             return true;
         case LT(_RAISE, KC_NO):
             if(record->tap.count && record->event.pressed) {
-                tap_code16(WBSPC);
-                return false;
-            }
-            return true;
-        case LT(_M_RAISE, KC_NO):
-            if(record->tap.count && record->event.pressed) {
-                tap_code16(M_WBSPC);
-                return false;
+                if (isWin) {
+                    register_code16(LCTL(KC_BSPC));
+                } else {
+                    register_code16(LALT(KC_BSPC));
+                }
+            } else {
+                if (isWin) {
+                    unregister_code16(LCTL(KC_BSPC));
+                } else {
+                    unregister_code16(LALT(KC_BSPC));
+                }
             }
             return true;
     }
@@ -265,16 +352,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
+  bool isWin = get_highest_layer(default_layer_state) == _QWERTY;
   switch(combo_index) {
     case Q_SUPER:
       if (pressed) {
-        if (get_highest_layer(default_layer_state) == _QWERTY) {
+        if (isWin) {
             register_code(KC_LGUI);
         } else {
             register_code(KC_LCTL);
         }
       } else {
-        if (get_highest_layer(default_layer_state) == _QWERTY) {
+        if (isWin) {
             unregister_code(KC_LGUI);
         } else {
             unregister_code(KC_LCTL);
@@ -286,8 +374,6 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LGUI_T(KC_QUOT):
-        case LCTL_T(KC_QUOT):
         case LSFT_T(KC_Z):
         case LALT_T(KC_X):
         case LALT_T(KC_DOT):
--- sofle/macros.h
+++ sofle/macros.h
@@ -4,17 +4,3 @@
 // Common workspace
 #define PRV_WPC LCTL(LGUI(KC_LEFT)) // previous workspace, combo for ctrl+super+left
 #define NXT_WPC LCTL(LGUI(KC_RGHT)) // next workspace, combo for ctrl+super+right
-
-// Windows
-#define WDEL LCTL(KC_DELETE) // delete word
-#define WBSPC LCTL(KC_BSPC) // delete word backspace
-#define NXTWD LCTL(KC_RIGHT) // next word
-#define PRVWD LCTL(KC_LEFT) // previous word
-
-// Macos
-#define M_WDEL LALT(KC_DELETE) 
-#define M_WBSPC LALT(KC_BSPC)
-#define M_NXTWD LALT(KC_RIGHT)
-#define M_PRVWD LALT(KC_LEFT)
-#define M_HOME RCMD(KC_LEFT)
-#define M_END RCMD(KC_RIGHT)
